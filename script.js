6f447708092c9d6b8e5de73c4113c0acc981671b
Technical Stark <vanshofficial606@gmail.com>
2024-06-19 02:31:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let sliding = document.getElementById('sliding')
+sliding.addEventListener('click', function run(){
+    sliding.className = 'make_slide'
+
+})
+
+
+// cars area ids'
+let audi = document.getElementById('audi')
+let bmw = document.getElementById('bmw')
+let ferrari = document.getElementById('ferrari')
+let ford = document.getElementById('ford')
+let koenigsegg = document.getElementById('koenigsegg')
+let nissan = document.getElementById('nissan')
+let porsche = document.getElementById('porsche')
+let toyota = document.getElementById('toyota')
+
+// form area ids'
+
+let audi1 = document.getElementById('audi1')
+let bmw1 = document.getElementById('bmw1')
+let ferrari1 = document.getElementById('ferrari1')
+let ford1 = document.getElementById('ford1')
+let koenigsegg1 = document.getElementById('koenigsegg1')
+let nissan1 = document.getElementById('nissan1')
+let porsche1 = document.getElementById('porsche1')
+let toyota1 = document.getElementById('toyota1')
+
+
+audi.addEventListener('click', function run(){
+
+})
+
+
+bmw.addEventListener('click', function run(){
+    
+})
+
+
+ferrari.addEventListener('click', function run(){
+    
+})
+
+ford.addEventListener('click', function run(){
+    
+})
+
+
+porsche.addEventListener('click', function run(){
+    
+})
+
+
+nissan.addEventListener('click', function run(){
+    
+})
+
+
+toyota.addEventListener('click', function run(){
+    
+})
+
+
+koenigsegg.addEventListener('click', function run(){
+    
+})
+
+
+audi.addEventListener('click', function run(){
+    
+})
